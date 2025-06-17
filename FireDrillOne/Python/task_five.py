e59873fc18efa01fd21f7bded6883154aa6dfcb7
justman0898 <jayigboneme@gmail.com>
2025-06-17 09:30:40 -0700

    push

---
+++ FireDrillOne/Python/task_five.py
@@ -0,0 +1,18 @@
+temp = 1
+counter = 0
+while true:
+	temp = temp * 4;
+	print(temp)
+	counter+=1;
+	ifcounter == 5: 
+		break; 
+
+temp = 1;
+counter = 0;				
+while true:
+	temp = temp * 8;
+	print(temp)
+	counter+=1;
+	
+	if counter == 5:
+		break; 
